7c04cd9cf5ece84c4c2d800088a1b0c0b3be3c7f
David Beauchesne <david.beauchesne@uqar.ca>
2022-09-02 11:33:12 -0400

    my structure suggestion

---
+++ _pipeline.R
@@ -0,0 +1,13 @@
+pipeline <- function() {
+  # Data 
+  source("code/data/01-data-download.R")
+  source("code/data/02-data-crop.R")
+  source("code/data/03-data-export.R")
+  
+  # Analyses 
+  # I guess you see where I'm going with his.. :) 
+  
+  # Figures 
+  
+  # Report 
+}
