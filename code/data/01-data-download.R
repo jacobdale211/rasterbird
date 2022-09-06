5a2bd759dfd43960ee43b093eb086a8877f456d6
Jacob Dale <jbd211@gmail.com>
2022-09-06 10:04:04 -0400

    adding code

--- code/data/01-data-download.R
+++ code/data/01-data-download.R
@@ -1,1 +1,3 @@
-#test
+path <- dir("/Users/jacobdale/tryitout/rasterbird/data/data-raw/", pattern = ".tif", full.names = TRUE)
+dat <- lapply(path, read_stars, proxy = TRUE)
+dat <- lapply(dat, st_transform, crs = 4326)
