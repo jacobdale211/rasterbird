ebad4a3e71224e52a3c6ef32866ae1c8a2ee674b
Jacob Dale <jbd211@gmail.com>
2022-09-06 10:56:07 -0400

    more code

--- code/figures/01-studyarea.R
+++ code/figures/01-studyarea.R
@@ -1,1 +1,10 @@
-#test
+brange <- st_transform(brange, crs = st_crs(stressor))
+ext <- st_bbox(brange)
+stressor <- st_crop(stressor, ext)
+
+#grid
+brange_buffer <- st_buffer(brange, 10000)
+grid <- st_make_grid(brange_buffer, n = c(20,20))
+uid <- st_intersects(brange_buffer, grid) |> unlist() |> sort()
+uid
+grid <- grid[uid]
---
+++ code/figures/03-bird_distribution.R
@@ -0,0 +1,4 @@
+#placeholder cropping of brange
+brange_small = brange[1:1,]
+plot(brange_small)
+st_area(brange_small)
