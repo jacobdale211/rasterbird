65e36e410f480bb8e8020a73edeb0d92c2497dfb
Jacob Dale <jbd211@gmail.com>
2022-09-22 10:15:58 -0400

    using downloaded halpern data

--- code/data/01-data-download.R
+++ code/data/01-data-download.R
@@ -10,7 +10,7 @@ venter <- stars::read_stars("./data/data-raw/venter/Dryadv3/Maps/croplands2005.t
 halpern <- "4f84f0e3" # Global cumulative human impacts assessments in 2008 and 2013 
 venter <- "103a233e" # Terrestrial human footprint
 pipedat::pipedat(c(halpern, venter)) # Download data using pipedat package
-
+pipedat::pipedat("4f84f0e3", halpern_layers = c("inorganic","night_lights","invasives","plumes_fert","plumes_pest","population","shipping","ocean_pollution"))
 # This will download more data than what you actually need, but you can either create a seperate script called something like 02-data-format.R to format and select only the data that you wish to use, or do it in your 02-data-crop.R script. In both cases, you would then export your formatted data in data-format by line of code, not manually.
 
 # You could also use `file.remove()` to remove the .tif files that you do not need, directly in this script here. This is what I would do personally. 
--- code/figures/01-studyarea.R
+++ code/figures/01-studyarea.R
@@ -1,4 +1,4 @@
-stressor = dat_halpern[1]
+stressor = stars::read_stars("./data/data-raw/halpern_cea-4f84f0e3/halpern_cea-4f84f0e3-2008-inorganic.tif/")
 
 brange_small <- stars::st_transform(brange_small, crs = sf::st_crs(stressor))
 ext <- sf::st_bbox(brange_small)
