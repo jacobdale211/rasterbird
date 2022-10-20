54c3bd026b5f8cc4a426b8907794f25a62cf718a
Jacob Dale <jbd211@gmail.com>
2022-10-20 16:19:54 -0400

    bird grid

---
+++ code/data/05-bird-grid.R
@@ -0,0 +1,10 @@
+brange = sf::st_read("./data/data-raw/birdlife/birds_multistress/Bylot_non_breeding_range.shp")
+brange_small = brange[1,1]
+
+#grid
+brange_buffer <- sf::st_buffer(brange_small, 10000)
+grid <- sf::st_make_grid(brange_buffer, n = c(20,20))
+uid <- sf::st_intersects(brange_buffer, grid) |> unlist() |> sort()
+uid
+grid <- grid[uid]
+plot(grid)
