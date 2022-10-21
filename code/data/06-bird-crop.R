872d9ea7d13be0d9234f03bc76b215ffdc44d42a
David Beauchesne <david.beauchesne@uqar.ca>
2022-10-21 15:09:39 -0400

    crop and warp

---
+++ code/data/06-bird-crop.R
@@ -0,0 +1,28 @@
+source("./code/functions/check.R")
+source("./code/data/02-data-import.R")
+ras <- "./data/data-format/bird-grid/"
+crp <- "./data/data-format/bird-crop/"
+chk_create(crp)
+files <- dir(ras, pattern = ".tif$", full.names = TRUE)
+bgrid <- lapply(files, stars::read_stars)
+
+# Get bird names
+spnm <- lapply(bgrid, names)
+spnm <- gsub("bird-grid_", "", spnm) 
+spnm <- gsub(".tif", "", spnm) 
+
+# Loop to crop each stressor over each species range
+for(i in 1:length(bgrid)) {
+  crpsp <- paste0(crp,spnm[i],"/")
+  chk_create(crpsp)
+  for(j in 1:length(stress)) {
+    print(paste0("bird: ", i,"; stressor: ", j))  
+    grd <- sf::st_transform(
+      bgrid[[i]], 
+      crs = sf::st_crs(stress[[j]])
+    )
+    sf::st_crop(stress[[j]], grd) |>
+    stars::write_stars(paste0(crpsp,"bird-crop_",spnm[j],"-",names(stress[[j]])))
+  }
+}
+
---
+++ code/data/07-bird-warp.R
@@ -0,0 +1,40 @@
+source("./code/functions/check.R")
+# source("./code/data/02-data-import.R")
+ras <- "./data/data-format/bird-grid/"
+crp <- "./data/data-format/bird-crop/"
+wrp <- "./data/data-format/bird-warp/"
+chk_create(wrp)
+files <- dir(ras, pattern = ".tif$", full.names = TRUE)
+bgrid <- lapply(files, stars::read_stars)
+
+# Get bird names
+spnm <- lapply(bgrid, names)
+spnm <- gsub("bird-grid_", "", spnm) 
+spnm <- gsub(".tif", "", spnm) 
+
+# Loop to crop each stressor over each species range
+for(i in 1:length(bgrid)) {
+  # This is messy 
+  wrpsp <- paste0(wrp,spnm[i],"/")
+  crpsp <- paste0(crp,spnm[i],"/")
+  chk_create(wrpsp)
+  stress <- dir(crpsp, pattern = ".tif$", full.names = TRUE) |>
+            lapply(stars::read_stars)
+            
+  # Iterate over stressors
+  for(j in 1:length(stress)) {
+    print(paste0("bird: ", i,"; stressor: ", j))  
+    grd <- sf::st_transform(
+      bgrid[[i]], 
+      crs = sf::st_crs(stress[[j]])
+    )
+    sf::st_crop(stress[[j]], grd) |>
+    sf::st_transform(crs = 4326) |>
+    stars::write_stars(
+      paste0(
+        wrpsp,
+        gsub("bird-crop_","bird-warp_",names(stress[[j]]))
+      )
+    )
+  }
+}
