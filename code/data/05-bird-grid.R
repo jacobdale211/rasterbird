0bbedc4b99f92008a32b0690ba16bac26f7a4676
Jacob Dale <jbd211@gmail.com>
2022-10-21 13:52:25 -0400

    grid loop

--- code/data/05-bird-grid.R
+++ code/data/05-bird-grid.R
@@ -1,12 +1,21 @@
+source("./code/functions/check.R")
+
 brange = sf::st_read("./data/data-raw/birdlife/birds_multistress/Bylot_non_breeding_range.shp")
-brange_small = brange[1,1]
+path <- "./data/data-format/bird-grid/"
+chk_create(path)
+sp <- brange$species
 
-#grid
-brange_buffer <- sf::st_buffer(brange_small, 10000)
-grid <- sf::st_make_grid(brange_buffer, n = c(20,20))
-uid <- sf::st_intersects(brange_buffer, grid) |> unlist() |> sort()
-uid
-grid <- grid[uid]
+for(i in 1:nrow(brange)) {
+  brange_buffer <- sf::st_buffer(brange[i,], 10000)
+  on.exit(sf::sf_use_s2(TRUE), add = TRUE)
+  sf::sf_use_s2(FALSE)
+  grid <- sf::st_make_grid(brange_buffer, n = c(20,20))
+  uid <- sf::st_intersects(brange_buffer, grid) |> unlist() |> sort()
+  grid <- grid[uid]
+  grid <- stars::st_as_stars(grid)  
+  stars::write_stars(
+    grid, 
+    paste0("./data/data-format/bird-grid/bird-grid_",sp[i],".tif")
+  )
+}
 
-grid <- stars::st_as_stars(grid)
-stars::write_stars(grid, "./data/data-format/bird-grid.tif/")
