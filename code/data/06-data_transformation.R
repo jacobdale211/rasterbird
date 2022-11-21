6580eb314c9591e31c0ac3d8c31eb132e64eba15
Jacob Dale <jbd211@gmail.com>
2022-11-21 14:51:41 -0500

    loop for cumulative plots

--- code/data/06-data_transformation.R
+++ code/data/06-data_transformation.R
@@ -34,18 +34,24 @@ for(i in 1:length(stressor_names)) {
   for(j in 1:length(stress)) stress[[j]] <- log(stress[[j]] + 1)
   
   # Evaluate 99th percentile for stressor i across all birds
-  vals <- list()
-  for (j in 1:length(stress)) {
-    r <- as.data.frame(stress[[j]]) 
-    vals[[j]] <- r[,3] 
+  # vals <- list()
+  # for (j in 1:length(stress)) {
+  #   r <- as.data.frame(stress[[j]]) 
+  #   vals[[j]] <- r[,3] 
+  # }
+  # quantile99 <- unlist(vals) |> quantile(probs = .99, na.rm = T) 
+  # 
+  # # Standardize individual bird rasters based on 99th percentile of stressor i across all birds
+  # for(j in 1:length(stress)) stress[[j]] <- stress[[j]] / quantile99
+  
+  # Standardize 
+  for(j in 1:length(stress)) {
+    md <- max(stress[[j]][[1]], na.rm = TRUE)
+    stress[[j]] <- stress[[j]]/md
   }
-  quantile99 <- unlist(vals) |> quantile(probs = .99, na.rm = T) 
-
-  # Standardize individual bird rasters based on 99th percentile of stressor i across all birds
-  for(j in 1:length(stress)) stress[[j]] <- stress[[j]] / quantile99
   
   # Export 
-  newfiles <- gsub("bird-warp","bird-transformed",files)
+  newfiles <- gsub("bird-warp","bird-transformed", files)
   for(j in 1:length(stress)) {
     stars::write_stars(
       stress[[j]],
--- code/data/07-cumulative_exposure.R
+++ code/data/07-cumulative_exposure.R
@@ -1,7 +1,30 @@
+source("./code/functions/check.R")
+plt <-"figures/cumulative/"
+chk_create(plt)
+
+str <- "figures/stressors/"
+chk_create(str)
+
+ras <- "./data/data-format/bird-grid/"
+files <- dir(ras, pattern = ".tif$", full.names = TRUE)
+bgrid <- lapply(files, stars::read_stars)
+
+# Names for plots
+cumul_nam <- lapply(bgrid, names)
+cumul_nam <- gsub("bird-grid_", "cumulative_", cumul_nam) 
+cumul_nam <- gsub(".tif", "", cumul_nam) 
+
+# Get bird names
+spnm <- lapply(bgrid, names)
+spnm <- gsub("bird-grid_", "", spnm) 
+spnm <- gsub(".tif", "", spnm) 
+
 library(stars)
 birdfolders <- dir("data/data-format/bird-transformed", full.names = TRUE)
 
 for(i in 1:length(birdfolders)) {
+  input <- cumul_nam[[i]]
+  
   dat <- dir(
     birdfolders[i],
     pattern = ".tif$", 
@@ -9,11 +32,13 @@ for(i in 1:length(birdfolders)) {
   ) |>
   lapply(stars::read_stars, proxy = FALSE)
   
-  
   # Cumulative plots of everything 
   cumul <- do.call("c", dat) |>
            st_redimension() |>
            st_apply(c(1,2), sum, na.rm = TRUE)
-  plot(cumul, breaks = "equal")
-  image(cumul, col = viridis::viridis(100))
+  # Export
+  png(file = glue::glue("figures/cumulative/{input}.png"))
+ image(cumul, col = viridis::viridis(100))
+  dev.off()
 }
+  
